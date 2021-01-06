90be0c74d03e23787da2038f1ed0451e3ed8db82
SALIMRJ <SALIMRAJI2017@GMAIL.COM>
2021-01-06 16:36:20 +0200

    salim change

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,29 @@
 'use strict';
 //Welcoming
+var grade=0 ; 
 var userName = prompt("what is your name?");
 alert("Welcome " + userName);
 confirm("lets play guessing game");
 //console.log(userName);
 
+FirstTest(); 
+Q6();
+Q7(); 
+
+
+
+
+
+
+
+
+function FirstTest(){
 //firs qustion
 var favTeam = prompt(" Is Barcelona my favourate football team?");
 if (favTeam.toLowerCase() === "yes" || favTeam.toLowerCase() === "y") {
    //console.log("!!!!!True!!!!");
    alert("!!!!!True!!!!")
-   var counter=1;
+   counter();
 }
 else if (favTeam.toLowerCase() === null || favTeam.toLowerCase() === "") {
    //console.log("why you did not answer?! ");
@@ -21,6 +34,64 @@ else {
    console.log("wrong");
    alert("wrong");
 }
+FirstTest2();
+}
+
+
+
+
+
+
+
+//console.log("Welcome again "+userName);
+alert("Welcome again " + userName);
+
+//end of lab one
+
+// 6th question
+function Q6(){
+   var trueAnswer = 3;
+   for (var i = 1; i <= 4; i++) {
+      var guissNumber = prompt("I put a number in my head, can you guess what it?");
+      if (parseInt(guissNumber) === 3) { alert("Amazing!!!!"); counter(); break; }
+      if (parseInt(guissNumber) <= 2) {
+         alert("too low");
+      }
+      if (parseInt(guissNumber) >= 4) {
+         alert("too high");}
+         if (i === 4) { alert("the answer is 3!"); }
+      
+   }
+}
+
+
+// 7th question
+function Q7(){
+
+   var countriesList = ['JORDAN', 'ENGLAND', 'UAE', 'USA', 'LEBANON', 'PALASTINE'];
+
+   var correct = false;
+   var j = 0;
+   while (j <= 5 && correct === false) {
+      var favcountry = prompt('Guess what is my fovourate countries? ALL LETTERS ARE CAPITAL');
+      for (var js = 0; js < countriesList.length; js++) {
+   
+         if (favcountry.toLowerCase() === countriesList[js].toLowerCase()) {
+            alert("TRUE");
+            var correct = true;
+            counter();
+            break;
+         }
+      }
+      j++;
+   }
+   alert('THE ANSWERS '+'JORDAN, ENGLAND, UAE, USA, LEBANON, PALASTINE');
+   alert('YOUR SCOORE '+grade.toString()+' from 7');
+}
+
+function FirstTest2(){
+
+   
 //second quesstion
 var fruit = prompt("Do I like Apple?");
 if (fruit.toLowerCase() === "yes" || fruit.toLowerCase() === "y") {
@@ -30,18 +101,25 @@ if (fruit.toLowerCase() === "yes" || fruit.toLowerCase() === "y") {
 else if (fruit.toLowerCase() === "no" || fruit.toLowerCase() === "n") {
    //console.log("you are good!");
    alert("you are good!");
-   counter++;
+   counter();
 }
 else {
    //console.log("-1; you did not answer!!");
    alert("-1; you did not answer!!");
 }
-// third question
+FirstTest3();
+
+}
+
+
+function FirstTest3(){
+
+   // third question
 var movie = prompt("Is interstellar my favourate movie?");
 if (movie.toLowerCase() === "yes" || movie.toLowerCase() === "y") {
    //console.log("true");
    alert("true");
-   counter++;
+   counter();
 }
 else if (movie.toLowerCase() === "" || movie.toeLowerCase() === null) {
    //console.log("you did not answer");
@@ -51,23 +129,31 @@ else {
    //console.log("false");
    alert("false");
 }
-// 4th question
-var facTecCompany = prompt(" Is apple my favourate company?")
-if (facTecCompany.toLowerCase() === "y" || facTecCompany.toLowerCase() === "y") {
-   //console.log("yes,I like its products");
-   alert("yes,I like its products");
-   counter++;
-}
-else if (facTecCompany.toLowerCase() === null || facTecCompany.toLowerCase() === "") {
-   //console.log("empty answer!");
-   alert("empty answer!");
-}
-else {
-   //console.log("false ");
-   alert("false");
+
+FirstTest4();
 }
 
-//5th question
+
+function FirstTest4(){
+   // 4th question
+   var facTecCompany = prompt(" Is apple my favourate company?")
+   if (facTecCompany.toLowerCase() === "y" || facTecCompany.toLowerCase() === "y") {
+      //console.log("yes,I like its products");
+      alert("yes,I like its products");
+      counter();
+   }
+   else if (facTecCompany.toLowerCase() === null || facTecCompany.toLowerCase() === "") {
+      //console.log("empty answer!");
+      alert("empty answer!");
+   }
+   else {
+      //console.log("false ");
+      alert("false");
+   }
+   FirstTest5();
+}
+function FirstTest5(){
+   //5th question
 var basketBall = prompt("Do I like BasketBall yes or no ?")
 if (basketBall.toLowerCase() === "yes" || basketBall.toLowerCase() === "y") {
    //console.log("no I do not");
@@ -76,58 +162,26 @@ if (basketBall.toLowerCase() === "yes" || basketBall.toLowerCase() === "y") {
 else if (basketBall.toLowerCase() === "no" || basketBall.toLowerCase() === "n") {
    //console.log("true");
    alert("true");
-   counter++;
+   counter();
 }
 else {
    //console.log("-1 for empty asnwer and random");
    alert("-1 for empty and rondom answer");
 }
-
-//console.log("Welcome again "+userName);
-alert("Welcome again " + userName);
-
-//end of lab one
-
-// 6th question
-var trueAnswer = 3;
-for (var i = 1; i <= 4; i++) {
-   var guissNumber = prompt("I put a number in my head, can you guess what it?");
-   if (parseInt(guissNumber) === 3) { alert("Amazing!!!!"); counter++; break; }
-   if (parseInt(guissNumber) <= 2) {
-      alert("too low");
-   }
-   if (parseInt(guissNumber) >= 4) {
-      alert("too high");}
-      if (i === 4) { alert("the answer is 3!"); }
-   
 }
-// 7th question
-var countriesList = ['JORDAN', 'ENGLAND', 'UAE', 'USA', 'LEBANON', 'PALASTINE'];
 
-var correct = false;
-var j = 0;
-while (j <= 5 && correct === false) {
-   var favcountry = prompt('Guess what is my fovourate countries? ALL LETTERS ARE CAPITAL');
-   for (var js = 0; js < countriesList.length; js++) {
+function AlertAll(text1,text2){
 
-      if (favcountry === countriesList[js]) {
-         alert("TRUE");
-         var correct = true;
-         counter++;
-         break;
-      }
-   }
-   j++;
+   alert(text1,text2);
+
+
+
 }
-alert('THE ANSWERS '+'JORDAN, ENGLAND, UAE, USA, LEBANON, PALASTINE');
-alert('YOUR SCOORE'+counter);
 
 
 
 
+function counter(){
+grade++;
 
-
-
-
-
-
+}
