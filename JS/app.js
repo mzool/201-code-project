d8d1bb95730e97feaf70cbb6850f21a9bc25e49f
mzool <mohammad_alzoul97@yahoo.com>
2021-01-09 14:32:20 +0200

    modifying

--- JS/app.js
+++ JS/app.js
@@ -1,55 +1,190 @@
 'use strict';
 //Welcoming
-var grade=0 ; 
+var grade = 0;
 var userName = prompt("what is your name?");
+while (userName === null || userName === "") { var userName = prompt("what is your name?"); }
 alert("Welcome " + userName);
-confirm("lets play guessing game");
-//console.log(userName);
+confirm("lets play yes/no game");
 
-FirstTest(); 
-Q6();
-Q7(); 
+function yes(Inputs) {
+   var userInputs = Inputs;
+   while (userInputs.toLowerCase() !== 'yes' && userInputs.toLowerCase() !== 'y' && userInputs.toLowerCase() !== 'no' && userInputs.toLowerCase() !== 'n') { var userInputs = prompt('Invalid answer!'); }
+   if (userInputs.toLowerCase() === 'yes' || userInputs.toLowerCase() === 'y') { alert("True"); grade++; }
+   else { alert("False"); }
+}
 
+function no(Inputs) {
+   var userInputs = Inputs;
+   while (userInputs.toLowerCase() !== 'yes' && userInputs.toLowerCase() !== 'y' && userInputs.toLowerCase() !== 'no' && userInputs.toLowerCase() !== 'n') { var userInputs = prompt('Invalid answer!'); }
+   if (userInputs.toLowerCase() === 'no' || userInputs.toLowerCase() === 'n') { alert("True"); grade++; }
+   else { alert("False"); }
 
-
-
-
-
-
-
-function FirstTest(){
-//firs qustion
+}
+// First five questions:
 var favTeam = prompt(" Is Barcelona my favourate football team?");
-if (favTeam.toLowerCase() === "yes" || favTeam.toLowerCase() === "y") {
-   //console.log("!!!!!True!!!!");
-   alert("!!!!!True!!!!")
-   counter();
-}
-else if (favTeam.toLowerCase() === null || favTeam.toLowerCase() === "") {
-   //console.log("why you did not answer?! ");
-   alert("why you did not answer?! ");
-}
+yes(favTeam);
+var TecCompany = prompt(" Is apple my favourate company?");
+no(TecCompany);
+var fruit = prompt("Do I like Orange?");
+yes(fruit);
+var movie = prompt("Is interstellar my favourate movie?");
+yes(movie);
+var basketBall = prompt("Do I like BasketBall yes or no ?");
+no(basketBall);
 
-else {
-   console.log("wrong");
-   alert("wrong");
-}
-FirstTest2();
-}
-
-
-
-
-
-
-
-//console.log("Welcome again "+userName);
-alert("Welcome again " + userName);
-
-//end of lab one
 
 // 6th question
-function Q6(){
+q6();
+function q6() {
+   var trueAnswer = 3;
+   for (var i = 1; i <= 4; i++) {
+      var guissNumber = prompt("I put a number in my head, can you guess what it?");
+      if (parseInt(guissNumber) === 3) { alert("Amazing!!!!"); grade++; break; }
+      if (parseInt(guissNumber) <= 2) {
+         alert("too low");
+      }
+      if (parseInt(guissNumber) >= 4) {
+         alert("too high");
+      }
+      if (i === 4) { alert("the answer is 3!"); }
+
+   }
+}
+q7();
+function q7() {
+   var countriesList = ['JORDAN', 'ENGLAND', 'UAE', 'USA', 'LEBANON', 'PALASTINE'];
+   var correct = false;
+   var j = 0;
+   while (j <= 5 && correct === false) {
+      var favcountry = prompt('Guess what is my fovourate countries?');
+      for (var js = 0; js < countriesList.length; js++) {
+         if (favcountry.toUpperCase() === countriesList[js].toUpperCase()) {
+            alert("TRUE");
+            var correct = true;
+            grade++;
+            break;
+         }
+      }
+      j++;
+   }
+}
+
+alert('THE ANSWERS ' + 'JORDAN, ENGLAND, UAE, USA, LEBANON, PALASTINE');
+alert("Your grade is " + grade + ' from 7');
+
+
+
+
+
+
+//console.log(userName);
+/*firstTest();
+firstTest2();
+firstTest3();
+firstTest4();
+firstTest5();
+q6();
+q7();*/
+/*function firstTest() {
+   //firs qustion
+   var favTeam = prompt(" Is Barcelona my favourate football team?");
+   if (favTeam.toLowerCase() === "yes" || favTeam.toLowerCase() === "y") {
+      alert("!!!!!True!!!!");
+        //console.log("!!!!!True!!!!");
+      counter();
+   }
+   while (favTeam.toLowerCase() === null || favTeam.toLowerCase() === "") {
+      //console.log("why you did not answer?! ");
+      var favTeam = prompt(" Is Barcelona my favourate football team?");
+   }
+
+   if (favTeam.toLowerCase() === "no" || favTeam.toLowerCase() === "n") {
+      console.log("wrong");
+      alert("wrong");
+   }
+}
+function firstTest2() {
+   //second quesstion
+   var fruit = prompt("Do I like Apple?");
+   if (fruit.toLowerCase() === "yes" || fruit.toLowerCase() === "y") {
+      alert("no I do not ");
+          //console.log("no I do not");
+   }
+   if (fruit.toLowerCase() === "no" || fruit.toLowerCase() === "n") {
+      alert("you are good!");
+         //console.log("you are good!");
+      counter();
+   }
+   while (fruit === null || fruit === "") {
+      var fruit = prompt("Do I like Apple?");
+      //console.log("-1; you did not answer!!");
+   }
+
+}
+function firstTest3() {
+
+   // third question
+
+   var movie = prompt("Is interstellar my favourate movie?");
+   if (movie.toLowerCase() === "yes" || movie.toLowerCase() === "y") {
+
+      alert("true");
+      counter();
+      //console.log("true");
+   }
+   while (movie.toLowerCase() === "" || movie.toeLowerCase() === null) {
+      var movie = prompt("Is interstellar my favourate movie?");
+   }
+   if (movie.toLowerCase() === "no" || movie.toLowerCase() === "n") {
+
+      alert("false");
+      //console.log("false");
+   }
+
+
+}
+
+function firstTest4() {
+   // 4th question
+   var facTecCompany = prompt(" Is apple my favourate company?")
+   if (facTecCompany.toLowerCase() === "y" || facTecCompany.toLowerCase() === "y") {
+      //console.log("yes,I like its products");
+      alert("yes,I like its products");
+      counter();
+   }
+   while (facTecCompany.toLowerCase() === null || facTecCompany.toLowerCase() === "") {
+      var facTecCompany = prompt(" Is apple my favourate company?")
+      //console.log("empty answer!");
+   }
+   if ((facTecCompany.toLowerCase() === "n" || facTecCompany.toLowerCase() === "no")){
+      alert("false");
+      //console.log("false ");
+   }
+
+}
+function firstTest5() {
+   //5th question
+   var basketBall = prompt("Do I like BasketBall yes or no ?")
+   if (basketBall.toLowerCase() === "yes" || basketBall.toLowerCase() === "y") {
+     alert("no I do not");
+       //console.log("no I do not");
+   }
+   if (basketBall.toLowerCase() === "no" || basketBall.toLowerCase() === "n") {
+      alert("true");
+      //console.log("true");
+      counter();
+   }
+   while(basketBall.toLowerCase() === "" || basketBall.toLowerCase() === null) {
+      var basketBall = prompt("Do I like BasketBall yes or no ?");
+   }
+}
+
+
+
+
+
+// 6th question
+function q6() {
    var trueAnswer = 3;
    for (var i = 1; i <= 4; i++) {
       var guissNumber = prompt("I put a number in my head, can you guess what it?");
@@ -58,15 +193,16 @@ function Q6(){
          alert("too low");
       }
       if (parseInt(guissNumber) >= 4) {
-         alert("too high");}
-         if (i === 4) { alert("the answer is 3!"); }
-      
+         alert("too high");
+      }
+      if (i === 4) { alert("the answer is 3!"); }
+
    }
 }
 
 
 // 7th question
-function Q7(){
+function q7() {
 
    var countriesList = ['JORDAN', 'ENGLAND', 'UAE', 'USA', 'LEBANON', 'PALASTINE'];
 
@@ -75,7 +211,7 @@ function Q7(){
    while (j <= 5 && correct === false) {
       var favcountry = prompt('Guess what is my fovourate countries? ALL LETTERS ARE CAPITAL');
       for (var js = 0; js < countriesList.length; js++) {
-   
+
          if (favcountry.toLowerCase() === countriesList[js].toLowerCase()) {
             alert("TRUE");
             var correct = true;
@@ -85,103 +221,11 @@ function Q7(){
       }
       j++;
    }
-   alert('THE ANSWERS '+'JORDAN, ENGLAND, UAE, USA, LEBANON, PALASTINE');
-   alert('YOUR SCOORE '+grade.toString()+' from 7');
+   alert('THE ANSWERS ' + 'JORDAN, ENGLAND, UAE, USA, LEBANON, PALASTINE');
+   alert('YOUR SCOORE ' + grade.toString() + ' from 7');
 }
+counter();
+function counter() {
+   grade++;
 
-function FirstTest2(){
-
-   
-//second quesstion
-var fruit = prompt("Do I like Apple?");
-if (fruit.toLowerCase() === "yes" || fruit.toLowerCase() === "y") {
-   //console.log("no I do not");
-   alert("no I do not ");
-}
-else if (fruit.toLowerCase() === "no" || fruit.toLowerCase() === "n") {
-   //console.log("you are good!");
-   alert("you are good!");
-   counter();
-}
-else {
-   //console.log("-1; you did not answer!!");
-   alert("-1; you did not answer!!");
-}
-FirstTest3();
-
-}
-
-
-function FirstTest3(){
-
-   // third question
-var movie = prompt("Is interstellar my favourate movie?");
-if (movie.toLowerCase() === "yes" || movie.toLowerCase() === "y") {
-   //console.log("true");
-   alert("true");
-   counter();
-}
-else if (movie.toLowerCase() === "" || movie.toeLowerCase() === null) {
-   //console.log("you did not answer");
-   alert("you did not answer");
-}
-else {
-   //console.log("false");
-   alert("false");
-}
-
-FirstTest4();
-}
-
-
-function FirstTest4(){
-   // 4th question
-   var facTecCompany = prompt(" Is apple my favourate company?")
-   if (facTecCompany.toLowerCase() === "y" || facTecCompany.toLowerCase() === "y") {
-      //console.log("yes,I like its products");
-      alert("yes,I like its products");
-      counter();
-   }
-   else if (facTecCompany.toLowerCase() === null || facTecCompany.toLowerCase() === "") {
-      //console.log("empty answer!");
-      alert("empty answer!");
-   }
-   else {
-      //console.log("false ");
-      alert("false");
-   }
-   FirstTest5();
-}
-function FirstTest5(){
-   //5th question
-var basketBall = prompt("Do I like BasketBall yes or no ?")
-if (basketBall.toLowerCase() === "yes" || basketBall.toLowerCase() === "y") {
-   //console.log("no I do not");
-   alert("no I do not");
-}
-else if (basketBall.toLowerCase() === "no" || basketBall.toLowerCase() === "n") {
-   //console.log("true");
-   alert("true");
-   counter();
-}
-else {
-   //console.log("-1 for empty asnwer and random");
-   alert("-1 for empty and rondom answer");
-}
-}
-
-function AlertAll(text1,text2){
-
-   alert(text1,text2);
-
-
-
-}
-
-
-
-
-function counter(){
-grade++;
-
-}
+}*/
